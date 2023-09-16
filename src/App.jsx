c01d495445e7a38fd4c7f11fbc23c3f9de97d385
sajib37 <sajibhasan9811@gmail.com>
2023-09-16 10:47:47 +0600

    Empty project created and tailwind linked, google font linked succesfull

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import AllCourses from './Components/AllCourses/AllCourses'
+
+function App() {
+
+  return (
+    <>
+      <AllCourses></AllCourses>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/AllCourses/AllCourses.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AllCourses = () => {
+    return (
+        <div>
+            <h1>My name is khan</h1>
+        </div>
+    );
+};
+
+AllCourses.propTypes = {
+    
+};
+
+export default AllCourses;
